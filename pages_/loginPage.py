b4b2eb1e30538aa559a8d9a56fbc451faad87392
Lilit <lilmankan@gmail.com>
2023-11-14 22:25:43 +0400

    Amazontests

--- pages_/loginPage.py
+++ pages_/loginPage.py
@@ -3,37 +3,40 @@ from selenium import webdriver
 from time import sleep
 from pages_.basePage import BasePage
 class LoginPage(BasePage):
-
     def __init__(self, driver: webdriver.Chrome):
         super().__init__(driver)
         self.__usernameFieldLocator = (By.ID, "ap_email")
         self.__continueButtonLocator = (By.ID, "continue")
         self.__passwordButtonLocator = (By.ID, "ap_password")
         self.__signInButtonLocator = (By.ID, "signInSubmit")
-
-
+        self.__changeMailButtonLocator = (By.ID, "ap_change_login_claim")
+        self.__detailsButtonLocator = (By.ID, "remember_me_learn_more_link")
+        self.__forgotYourPasswordButtonLocator = (By.ID, "auth-fpp-link-bottom")
     def fill_username_field(self, username):
         userNameFieldElement = self._find_element(self.__usernameFieldLocator)
         self._fill_field(userNameFieldElement, username)
-
-
     def click_to_continue_button(self):
         continueButtonElement = self._find_element(self.__continueButtonLocator)
         self._click(continueButtonElement)
-
     def fill_password_field(self, password):
         passwordFieldElement = self._find_element(self.__passwordButtonLocator)
         self._fill_field(passwordFieldElement, password)
-
-
     def click_to_signin_button(self):
-        sleep(6)
         signinButtonElement = self._find_element(self.__signInButtonLocator)
         self._click(signinButtonElement)
-        sleep(6)
-
+    def click_to_change_button(self):
+        changeButtonLocator = self._find_element(self.__changeMailButtonLocator)
+        self._click(changeButtonLocator)
+    def click_to_details_button(self):
+        clickDetailsButton = self._find_element(self.__detailsButtonLocator)
+        self._click(clickDetailsButton)
+    def click_to_forgot_your_password_button(self):
+        clickForgotYourPassword = self._find_element(self.__forgotYourPasswordButtonLocator)
+        self._click(clickForgotYourPassword)
     def validate_continue_button_text(self):
         continueButtonElement = self._find_element(self.__continueButtonLocator)
         if self._get_text(continueButtonElement) != "continue":
             print("Error: Wrong continue button text")
             exit(2)
+
+
--- pages_/navigationBar.py
+++ pages_/navigationBar.py
@@ -1,13 +1,21 @@
 from selenium.webdriver.common.by import By
 from pages_.basePage import BasePage
-
-
 class NavigationBar(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self._cartButtonLocator = (By.ID, "nav-cart-count-container")
-
+        self._searchButtonLocator = (By.ID, "twotabsearchtextbox")
+        self._amazonButtonLocator = (By.ID, "nav-logo-sprites")
+        self._allSearchButtonLocator = (By.ID, "searchDropdownBox")
     def click_to_cart_button(self):
         cartButtonElement = self.driver.find_element(self._cartButtonLocator)
         self._click(cartButtonElement)
-
+    def search_on_cart_button(self):
+        searchButtonElement = self.driver.find_element(self._searchButtonLocator)
+        self._click(searchButtonElement)
+    def click_to_amazon_button_locator(self):
+        amazonButtonLocator = self.driver.find_element(self._amazonButtonLocator)
+        self._click(amazonButtonLocator)
+    def click_all_search_button_locator(self):
+        allSearchButtonLocator = self.driver.find_element(self._allSearchButtonLocator)
+        self._click(allSearchButtonLocator)
